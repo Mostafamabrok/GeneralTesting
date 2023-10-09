8825f7e6d8cd77544cbe3dd1aa4d49d1bf04ee0f
Mostafamabrok <mostafa.m.mabrok@gmail.com>
2023-10-09 18:48:41 +0300

    Removed Comments for readibility's sake

--- C++/fourth.cpp
+++ C++/fourth.cpp
@@ -3,14 +3,7 @@
 using namespace std;
 
 int main() {
-    //Comment Testing
-    /* This is a multi-line comment
-    See? This is the second line.*/
-
-    //cout << "Hello World!\n";
-    //cout << "Its C++ time!";
-    //You don't actually need to assign the type while assigning the variable, you can assign it like: int VariableTestNumber, then assign a value to it later
-
+    
     int VariableTestNumber = 14;
     cout << VariableTestNumber << endl;
 
