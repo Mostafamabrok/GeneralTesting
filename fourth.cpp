b87243b920f03b7fdd8961f9f9de7a84413e2964
Mostafamabrok <mostafa.m.mabrok@gmail.com>
2023-10-09 18:05:04 +0300

    Playing around with C++

--- fourth.cpp
+++ fourth.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+
 using namespace std;
 
 int main() {
-  cout << "Hello World!";
-  return 0;
+    //Comment Testing
+    /* This is a multi-line comment
+    See? This is the second line.*/
+
+    //cout << "Hello World!\n";
+    //cout << "Its C++ time!";
+    //You don't actually need to assign the type while assigning the variable, you can assign it like: int VariableTestNumber, then assign a value to it later
+
+    int VariableTestNumber = 14;
+    cout << VariableTestNumber << endl;
+
+    string myText = "El momento del text es: ";
+
+    cout << myText << VariableTestNumber;
+
+    return 0;
 }
