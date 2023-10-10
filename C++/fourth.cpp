745d450c451ca575c743410e9ca3b8178a1618f4
MostafaMarbok <mostafa.m.mabrok@gmail.com>
2023-10-10 15:55:47 +0300

    Gave functionality to calculator_main().

--- C++/fourth.cpp
+++ C++/fourth.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
 #include <string>
 
-void calculator_main(){
-
-}
-
 void calculator_add(){
 
 }
@@ -17,8 +13,29 @@ void calculator_multiply(){
 
 }
 
-void caclulator_divide(){
+void calculator_divide(){
 
+}
+
+void calculator_main(){
+
+    std::cout << "Calculator version 1.0";
+
+    std::cout << "What would you like to do?\n(a)dd,\n(s)ubtract,\n(m)ultiply,\n(d)ivide,\n(e)exit:";
+
+    char action;
+    std::cin >> action;
+
+    if (action == 'a' ){calculator_add();}
+    if (action == 's' ){calculator_subtract();}
+    if (action == 'm' ){calculator_multiply();}
+    if (action == 'd' ){calculator_divide();}
+
+    if (action == 'e'){
+        std::cout << "Exiting...\n\n";
+    }
+    
+    else {calculator_main();}
 }
 
 int main() {
