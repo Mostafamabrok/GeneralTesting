ece9593bb7b670e05f1fc1f9f9d1d0c688ca7b0b
Mostafamabrok <mostafa.m.mabrok@gmail.com>
2024-07-27 23:51:37 +0300

    Started Learning C in the right place

---
+++ C/test_1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+int number_variable = 15;
+int comparison_variable = 25;
+//This is a comment
+int main() {
+  printf("Your number is: ");
+  printf("%d" ,number_variable);
+
+  if (number_variable > comparison_variable) {
+    printf("\n\nYour number is also greater than %d!", comparison_variable);
+
+  }
+  else {
+    printf("\n\nYour number is not greater than %d", comparison_variable);
+  }
+
+
+  return 0;
+}
